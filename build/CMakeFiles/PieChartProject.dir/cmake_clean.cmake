7a202c5232cc8d83d655aaa1fc69b7cb4b84efc5
Antony C <antony.coco@live.fr>
2023-07-31 11:35:52 +0200

    Commit dinitialisation projet

---
+++ build/CMakeFiles/PieChartProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PieChartProject.dir/main.c.o"
+  "CMakeFiles/PieChartProject.dir/main.c.o.d"
+  "PieChartProject"
+  "PieChartProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PieChartProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello Nino!\n");
+    return 0;
+}
